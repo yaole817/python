63d37a9363fbf5219f2f07b6893dca0982e76507
yaole <yaole817@163.com>
2017-01-20 22:02:25 +0800

    start learing https scraping
    Signed-off-by: yaole <yaole817@163.com>

--- html/getHTML.py
+++ html/getHTML.py
@@ -1,4 +1,6 @@
+# only support python 3 or later
 import urllib.request
+from bs4 import soup
 with urllib.request.urlopen('http://www.baidu.com') as response:
 	html = response.read()
 	print(html)
