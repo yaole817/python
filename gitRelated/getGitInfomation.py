71a2d6ca822a7cbb3ef65b740503d876e9d3db83
yaole <yaole817@163.com>
2017-01-22 09:57:11 +0800

    update get_git_change_files method
    Signed-off-by: yaole <yaole817@163.com>

--- gitRelated/getGitInfomation.py
+++ gitRelated/getGitInfomation.py
@@ -3,20 +3,20 @@ import re
 import time
 
 def findCommitId(string): #input is string
-	wholeCommitID	= re.findall(r'commit(.*?)\n',string)#get the line of commit id 
+	wholeCommitID	= re.findall(r'\ncommit (.*?)\n',string)#get the line of commit id 
 	commitIDFilter=[item.strip() for item in wholeCommitID if re.search('[0-9a-fA-F]{40}',item)]  #get commit id from the line and return list
 	return commitIDFilter
 def findCommitModule(string):
-	string=string.replace("\ncommit","\ncommitcommit")
-	allChangeFilesRegex=re.compile(r'commit\b[\s\S]+?\ncommit',re.DOTALL) #get commit log from one commit id,which include commit id,diff files, diff content etc.
+	string=string.replace("\ncommit","\ncommit\ncommit")
+	allChangeFilesRegex=re.compile(r'\ncommit\b[\s\S]+?\ncommit',re.DOTALL) #get commit log from one commit id,which include commit id,diff files, diff content etc.
 	commitInfoList = allChangeFilesRegex.findall(string) #return list by commit id
 	return commitInfoList
 	
 def findChangeFiles(string):
-	changeFilesReg	= re.compile(r'--git(.*?)index',re.DOTALL)#get the content of the change files in git log
+	changeFilesReg	= re.compile(r'diff --git(.*?)\n')#get the content of the change files in git log
 	
 	changeFiles		= changeFilesReg.findall(string)
-	changeFiles		= [item.split()[0][1:] for item in changeFiles]#split the change files
+	changeFiles		= [item.split()[1][1:] for item in changeFiles]#split the change files
 	return changeFiles
 	
 def findAuthor(string): #get author infomation
@@ -24,13 +24,13 @@ def findAuthor(string): #get author infomation
 	authorContent = [re.search(r'<(.*?)>',item).group()[1:-1] for item in authorReg.findall(string)]
 	return authorContent
 	
-def findTime(string):
-	timePattern	= re.compile(r"[\s\S]{3} [\s\S]{3} [0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4}")
-	commitTime	= [time.strftime('%Y-%m-%d %H:%M:%S',time.strptime(item.split('\n')[0].replace('Date:','').strip(),r'%a %b %d %H:%M:%S %Y')) for item in timePattern.findall(string)]
+def findTime(string): #get the time of commite, and transfer to standard format
+	timeReg		= re.compile(r"[\s\S]{3} [\s\S]{3} [0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4}")
+	commitTime	= [time.strftime('%Y-%m-%d %H:%M:%S',time.strptime(item.split('\n')[0].replace('Date:','').strip(),r'%a %b %d %H:%M:%S %Y')) for item in timeReg.findall(string)]
 	return commitTime
-def findCommitMessage(text):
+def findCommitMessage(string):
 	commitPartReg 	= re.compile(r'\bDate[\s\S]+?\n\n[\s\S]*\n\n',re.DOTALL)
-	commitContent	= [item.split('\n')[2].strip() for item in commitPartReg.findall(text)]
+	commitContent	= [item.split('\n')[2].strip() for item in commitPartReg.findall(string)]
 	return commitContent
 
 
@@ -45,8 +45,8 @@ if __name__ == "__main__":
 	commitModule=findCommitModule(string) #split modules by commit id 
 	#print(commitModule[0])
 	for item in commitModule:
-		print('{0:25}{1:25}{2:20}'.format(''.join(findTime(item)),''.join(findAuthor(item)),''.join(findCommitId(item))))
-		print('  commit message:'+''.join(findCommitMessage(item)))
+		print('{0:25}{1:25}{2:20}'.format(''.join(findTime(item)),''.join(findAuthor(item)),'\t'.join(findCommitId(item))))
+		print('    commit message:   '+''.join(findCommitMessage(item)))
 		for line in findChangeFiles(item):
 			print('    '+line)
 		#if r'Revert "Patch RTC read RxA-D patch"' in item:
