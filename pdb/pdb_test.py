7e26e7d150bb20af4b86b61c2b76ba833be63e34
yaole <yaole817@163.com>
2017-03-29 15:01:21 +0800

    add pdb debug
    Signed-off-by: yaole <yaole817@163.com>

---
+++ pdb/pdb_test.py
@@ -0,0 +1,12 @@
+# /usr/bin/python
+from __future__ import print_function
+import pdb
+def sum_nums(n):
+	s = 0
+	for i in range(n):
+		pdb.set_trace()
+		s+= i
+		print(s)
+
+if __name__ == '__main__':
+	sum_nums(10)
