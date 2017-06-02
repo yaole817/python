9efe4d8542a73b9ba27d2828a4492057be767ffd
yaole817 <yaole817@163.com>
2017-06-02 11:15:31 +0800

    refactoring the code and add readMe.md
    Signed-off-by: yaole817 <yaole817@163.com>

--- ParseIRS/ParseIRS.py
+++ ParseIRS/ParseIRS.py
@@ -1,12 +1,17 @@
+###
+#### python version: 2.7
+#### 
+
 from zipfile import ZipFile
 import re
+import sys
 
 
-class Docx2Text:
+class Docx2Text(object):
 	def __init__(self,docxName):
-		self._docxName = docxName
-		self.xml 	   = str(ZipFile(self._docxName).read('word/document.xml'))
-	
+		self.__docxName = docxName
+		self.xml 	    = str(ZipFile(self.__docxName).read('word/document.xml'))
+
 	def findParagraph(self,xml):
 		paragraphPattern= re.compile(r'.*(<w:p w)(.*?)(</w:p>)')
 		pargraphTuple	= paragraphPattern.findall(xml)
@@ -47,63 +52,73 @@ class Docx2Text:
 			s+=m[1]
 		return s
 
-class ParseIRS():
+
+
+
+class ParseIRS(Docx2Text):
 	def __init__(self,docxName):
-		self.irsName = docxName
+		Docx2Text.__init__(self,docxName)
+		self.__xml = self.xml
 		self.__startIndex = 0
-		self.__endIndex =0
-		self.__irs = Docx2Text(self.irsName)
+		self.__endIndex = 0
+		
 
 	def findOffsetAddr(self,xml):
-		return self.__irs.findParagraph(xml)
+		return super(ParseIRS,self).findParagraph(xml)
 
 	def parseOneTable(self,tableXml):
 		
-		self.__endIndex = self.__irs.xml.index(tableXml)
+		self.__endIndex = self.__xml.index(tableXml) 
 	
-		oneTable  = self.__irs.parseTable(tableXml)
-		newXml = self.__irs.xml[self.__startIndex:self.__endIndex]
+		oneTable  = super(ParseIRS,self).parseTable(tableXml)
+		newXml = self.__xml[self.__startIndex:self.__endIndex] # cut off the xml
 
-		paragraph = self.findOffsetAddr(newXml)[-1]
+		paragraph = self.findOffsetAddr(newXml)[-1]  # find the lastest paragraph,which is always the base address
 
-		offsetAddr = self.__irs.findText(paragraph)
+		offsetAddr = super(ParseIRS,self).findText(paragraph) 
 
+		self.__startIndex  = self.__endIndex
 		return offsetAddr,oneTable
 
 
 	def parseIrs(self):
-		tableXmls = self.__irs.findTable(self.__irs.xml)
+		resultTable = []
+		tableXmls = super(ParseIRS,self).findTable(self.__xml)
+		for tableXml in tableXmls:
+			offsetAddr,oneTable = self.parseOneTable(tableXml)
+			resultTable.append([offsetAddr,oneTable])
+		return resultTable
 
-		with open("debug.txt",'w+')as f:
-			for tableXml in tableXmls:
-				offsetAddr,oneTable = self.parseOneTable(tableXml)
-				f.write(offsetAddr)
+
+
+def writeFile(tableList,filename):
+	splitSign = ','
+
+	def writeOnetable(tablelist):
+		for row in tablelist:
+			if len(row)==11:
+				[bit,attr,hwPro,defau,descirption,mnem,chipRev,pwrDm,s,p,e]=row
+				f.write(splitSign+bit+splitSign+attr+splitSign+hwPro+splitSign+defau+splitSign+mnem+splitSign+pwrDm+splitSign+s+splitSign+p+splitSign+e+splitSign)
 				f.write('\n')
-				for row in oneTable:
-					f.write('|'.join(row))
-					f.write('\n')
-
-
-
-def writeFile(fileList,newFilename):
-	with open("debug.txt",'w+')as f:
-		for line in newFilename:
-			f.write(line)
+	with open(filename,'w+')as f:
+		for item in tableList:
+			offsetAddr = item[0]
+			oneTable   = item[1]
+			f.write(offsetAddr.replace('Default','\tDefault'))
 			f.write('\n')
+			writeOnetable(oneTable)
 
-docName		= 'IRS_CHX002_D18F0_xHCICtrl_R091.docx'
-resultList=[]
 
+
+
+docName		= sys.argv[1]
 if __name__ == "__main__":
 
+	if docName.split('.')[-1] != 'docx':
+		print('please input a docx file')
+		exit()
 
 	irsName = ParseIRS(docName)
-	irsName.parseIrs()
-
-
-
-	#for tableXml in tableXmls:
-	#	text= docx.findText(tableXml)
-	#print text
-	#writeFile('irs.txt',text[5])
-	#writeFile('irs.txt',text[7])
+	tableList = irsName.parseIrs()
+	writeFile(tableList,'irs1.txt')
+	print("parse done")
---
+++ ParseIRS/readMe.md
@@ -0,0 +1,18 @@
+# ParseIRS 使用方法
+
+1、首先将doc文件手动转换位docx文件，由于文本只能处理docx文件
+
+2、将docx文件路径赋值给脚本文件中的docName 变量，相对路径或绝对路径均可
+
+3、电脑上安装python2.7
+
+4、运行 python ParseIRS.py  irs_name，等几秒即可输出结果，例如：
+
+```
+python ParseIRS.py IRS_CHX002_D18F0_xHCICtrl_R091.docx
+
+或者
+
+python ParseIRS.py D:\CHX001_Tool\IRS_CHX002_D18F0_xHCICtrl_R091.docx
+```
+
