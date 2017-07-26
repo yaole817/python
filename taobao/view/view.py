4c06490bc808c6c8cb0a98f746d3d3caf11e209e
yaole817 <yaole817@163.com>
2017-07-26 19:22:19 +0800

    update: update UI method

--- taobao/view/view.py
+++ taobao/view/view.py
@@ -24,20 +24,21 @@
 
 from tkinter import *
 import tkinter.messagebox
+import tkinter as tk
 
 class MainWindow:
     def __init__(self):
         self.frame = Tk()
-        self.frame.minsize(380,530)
-        self.frame.maxsize(380,530)
+        self.frame.minsize(300,100)
+        self.frame.maxsize(300,100)
         self.frame.title('python ui') #set name 
 
         self.label_name = Label(self.frame,text = "name:")
         self.label_age  = Label(self.frame,text = "age:")
         self.label_sex  = Label(self.frame,text = "sex:")
 
-        self.text_name = Text(self.frame,height = "10",width = 30)
-        self.text_age  = Text(self.frame,height = "10",width = 30)
+        #self.text_name = Text(self.frame,height = "10",width = 30)
+        self.text_age  = Text(self.frame,height = "1",width = 30)
         self.text_sex  = Text(self.frame,height = "1",width = 30)
 
         self.label_name.grid(row = 0,column = 0)
@@ -47,7 +48,7 @@ class MainWindow:
         self.button_ok = Button(self.frame,text = "确定",width = 10)
         self.button_cancel = Button(self.frame,text = "cancel",width = 10)
 
-        self.text_name.grid(row = 0,column = 1)
+        #self.text_name.grid(row = 0,column = 1)
         self.text_age.grid(row = 1,column = 1)
         self.text_sex.grid(row = 2,column = 1)
         
@@ -56,10 +57,18 @@ class MainWindow:
 
         self.button_ok.bind("<ButtonRelease-1>",self.buttonListener1)  
         self.button_cancel.bind("<ButtonRelease-1>",self.buttonListener2)
+
+        self.entry_var = StringVar()
+        self.entry = tk.Entry(self.frame,textvariable = self.entry_var)
+        self.entry.grid(row =0,column = 1,padx = 15,pady =2)
+
         self.frame.mainloop()
 
     def buttonListener1(self,event):
-        tkinter.messagebox.showinfo("messagebox","this is button 1 dialog")  
+        #tkinter.messagebox.showinfo("messagebox","this is button 1 dialog")  
+        text = self.entry.get()
+        if len(text.strip())>0:
+        	print(text)
     def buttonListener2(self,event):
         tkinter.messagebox.showinfo("messagebox","this is button 2 dialog")
     def text(self,event):
