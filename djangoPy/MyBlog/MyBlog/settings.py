fa4cd4b53dead7ba36c982612797b046085dbaf0
yaole817 <yaole817@163.com>
2018-04-25 20:17:46 +0800

    add bootstrap
    Signed-off-by: yaole817 <yaole817@163.com>

--- djangoPy/MyBlog/MyBlog/settings.py
+++ djangoPy/MyBlog/MyBlog/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hans'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
