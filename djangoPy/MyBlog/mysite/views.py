805960861a7ab9558147491c33deb8452cc1f87f
yaole817 <yaole817@163.com>
2018-04-25 18:53:29 +0800

    add my blog project in django
    Signed-off-by: yaole817 <yaole817@163.com>

---
+++ djangoPy/MyBlog/mysite/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+from .models import Article
+import datetime
+
+def index(request):
+    blogAllList = Article.objects.all()
+    context = {}
+    # now = datetime.datetime.now()
+    # context['hello'] = "Hello World "
+    context['blogAllList'] = blogAllList
+    # context['date']  = 'date is %s' % now
+    
+    return render(request, 'home.html', context)
